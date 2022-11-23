c6113b2daa78e4dd444e7a28f993a2e9fabf2ec3
titulebolide <seneztit@gmail.com>
2022-11-23 23:13:59 +0100

    finish graph generation

--- step3.py
+++ step3.py
@@ -10,17 +10,18 @@ from bs4 import BeautifulSoup
 import re
 import pickle
 import os
+import numpy as np
 
 input_file = "../data/full/flights_post_processed.dat"
 
-with open(input_file, "r") as f:
+with open(input_file, "rb") as f:
     wings = pickle.load(f)
 
 class WingDetails:
     def __init__(self):
         self.cache_dir = "./cache"
         self.cache_filename = 'wing_details.dat'
-        self.cache_file = os.path.join(self.cache_dir,self.cachefile)
+        self.cachefile = os.path.join(self.cache_dir,self.cache_filename)
         self.cache = {}
         if not os.path.isdir(self.cache_dir):
             os.makedirs(self.cache_dir)
@@ -51,31 +52,66 @@ wd = WingDetails()
 wd.load_cache()
 data = []
 nbw = len(wings)
+classes = set()
 for now, wid in enumerate(wings):
-    if wings[wid]['dev'] > 3:
+    confidence = wings[wid]['confidence']
+    mean = wings[wid]['mean']
+    if confidence > 0.008:
         continue
-    if math.isnan(wings[wid]['dev']) or math.isnan(wings[wid]['mean']):
+    if math.isnan(confidence) or math.isnan(confidence):
         continue
-    print(wid, "-", end = "")
     name, clas = wd.get_wing_details(int(wid))
-    print(f"{now}/{nbw} - {name}")
+    classes.add(clas)
+    print(f"{now}/{nbw} - {name} (#{wid})")
     data.append((
         int(wid), #0
         name, #1
         clas, #2
-        utils.ga2gr(wings[wid]['mean']), #3
-        max(0, utils.ga2gr(wings[wid]['mean'] + wings[wid]['dev']) - utils.ga2gr(wings[wid]['mean'])), #4, upper error
-        max(0, utils.ga2gr(wings[wid]['mean']) - utils.ga2gr(wings[wid]['mean'] - wings[wid]['dev'])) #5, lower error
+        utils.ga2gr(mean), #3
+        max(0, utils.ga2gr(mean + confidence) - utils.ga2gr(mean)), #4, upper error
+        max(0, utils.ga2gr(mean) - utils.ga2gr(mean - confidence)) #5, lower error
     ))
 wd.write_cache()
 data.sort(key = lambda x : x[3])
 
-lower_error = [i[5] for i in data]
-upper_error = [i[4] for i in data]
-height = [i[3] for i in data]
-names = [i[1] for i in data]
+data = np.array(data)
 
-plt.subplots_adjust(bottom=0.3)
-plt.bar(list(range(len(data))), height, yerr = (lower_error, upper_error), tick_label=names)
-plt.xticks(rotation=90)
-plt.show()
+lower_error = data[:,5]
+upper_error = data[:,4]
+height = data[:,3]
+names = data[:,1]
+
+classes = list(classes)
+classes.sort()
+
+clas_to_name = {
+    "A": ("A", '#316625'), 
+    "B": ("B", '#37AD00'), 
+    "C": ("C", '#FFB300'), 
+    "D": ("D", '#ba2320'), 
+    "K": ("CCC", '#723277'), 
+    "O": ("No Certification", '#000000')
+}
+
+plt.figure(figsize=(9,20), dpi=400)
+plt.subplots_adjust(left=0.3,top=0.95, bottom=0.05)
+
+
+for clas in classes:
+    indexes = np.where(data[:,2] == clas)[0]
+
+    plt.barh(
+        indexes,
+        height[indexes].astype(float),
+        xerr = (lower_error[indexes].astype(float), upper_error[indexes].astype(float)),
+        label = clas_to_name[clas][0],
+        color = clas_to_name[clas][1]
+    )
+plt.yticks(np.arange(data.shape[0]),names, rotation=0, horizontalalignment="right", fontsize=7)
+plt.xlim(6.2, 8.1)
+plt.gca().invert_yaxis()
+plt.tick_params(labeltop=True)
+plt.legend()
+plt.grid(axis='x')
+plt.xlabel('Glide ratio')
+plt.savefig("graph.png")
